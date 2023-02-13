dd4edcb27f5541f1096b95cdf0d176daad79ebba
robert <X00204385@myTUDublin.ie>
2023-02-13 10:04:37 +0000

    Pretty print

--- dash.sh
+++ dash.sh
@@ -1,12 +1,10 @@
-while true
-do
-clear
-cat dash-header
-cat dash-carriage[0-9]
-cat dash-footer
-if [ -f driver.log ]
-then
-    tail -3 driver.log
-fi
-sleep 5
+while true; do
+    clear
+    cat dash-header
+    cat dash-carriage[0-9]
+    cat dash-footer
+    if [ -f driver.log ]; then
+        tail -3 driver.log
+    fi
+    sleep 5
 done
--- simple_sub.sh
+++ simple_sub.sh
@@ -3,29 +3,30 @@
 
 # subscribe to topic and process new lines one at a time in a while loop
 # note the v option gives us the topic AND the message
-mosquitto_sub -v -h localhost -t /simpletest/# | while read line
-do
-	# is this simple example all we do is echo the line to the screen
-  	echo $line 
-	# how about we cut out a field
-	topicF1=`echo $line |cut -f3 -d/`
-	echo topic field 1 is: $topicF1 
-	topicF2=`echo $line |cut -f4 -d/`
-	echo topic field 2 is: $topicF2
-	topicM=`echo $line |cut -f2 -d' '`
-	echo message: $topicM 
-	
-	case "$topicF1" in 
-	carriage[0-9])
-	    case "$topicM" in
-	    ON|OFF) echo "$topicF1: $topicM" > dash-$topicF1
-		;;
-	    *)
-		echo Received bad data 
-		;;
-	*)
-	    echo Received bad data 
-	    ;;
-	    esac
+mosquitto_sub -v -h localhost -t /simpletest/# | while read line; do
+    # is this simple example all we do is echo the line to the screen
+    echo $line
+    # how about we cut out a field
+    topicF1=$(echo $line | cut -f3 -d/)
+    echo topic field 1 is: $topicF1
+    topicF2=$(echo $line | cut -f4 -d/)
+    echo topic field 2 is: $topicF2
+    topicM=$(echo $line | cut -f2 -d' ')
+    echo message: $topicM
+
+    case "$topicF1" in
+    carriage[0-9])
+        case "$topicM" in
+        ON | OFF)
+            echo "$topicF1: $topicM" >dash-$topicF1
+            ;;
+        *)
+            echo Received bad data
+            ;;
+        *)
+            echo Received bad data
+            ;;
         esac
+        ;;
+    esac
 done
